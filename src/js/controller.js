b29a4a226a7baca279ef090ebbaba39f9b3a2f5f
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-01-07 00:22:52 +0700

    First request to Forkify API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+const { async } = require("q");
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +13,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe=async function(){
+  try{
+    //const respond=await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const respond=await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc89a');
+    const data=await respond.json();
+    //console.log(respond,data);
+    let {recipe}=data.data;
+    recipe={
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      imageUrl:recipe.image_url,
+      ingredients:recipe.ingredients,
+      publisher:recipe.publisher,
+      servings:recipe.servings,
+      sourceUrl:recipe.source_url,
+      title:recipe.title
+    }
+    //console.log(recipe,data.data.recipe);
+    if(!respond.ok){
+      throw new Error(`${respond.status} ${data.message}`)
+    }
+  }
+  catch(err){
+    alert(err);
+  }
+}
+showRecipe();
